6ef081a36b182dbb3d5ae4242ace2f91154f1896
Arnav Deep <arnavd4@gmail.com>
2023-05-21 20:10:30 +0530

    move counter

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,5 +1,3 @@
-import { counter } from './counter.js'
-
 $(function() {
   const d = new Date();
   const hours = d.getHours();
@@ -13,7 +11,6 @@ $(function() {
     body.classList.add('night');
   }
 
-  counter()
 
   toggle.addEventListener('click', function() {
     const isChecked = input.checked;
@@ -72,6 +69,96 @@ $(function() {
     scale: 1,
     viewFactor: 0.3,
   });
+
+  function counter() {
+
+    console.log('STarted')
+
+    const baseURL = 'https://be.letstranzact.com';
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+    "email": "arnav+inv@letstranzact.com",
+    "password": "123456"
+    });
+
+    const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(`${baseURL}/main/login/password-login/`, requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.error('eerre---', error))
+
+
+    // const url = `${baseURL}/inventory/main-inventory/bulk_manual_adjustment_or_stock_transfer/`;
+  
+    // const headers = {
+    //   "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR2iod4s",
+    //   "content-type": "application/json",
+    // };
+  
+    // const body = {
+    //   "fifo_status": true,
+    //   "from_store": null,
+    //   "to_store": 582317,
+    //   "comment": "",
+    //   "stockData": [{
+    //     "id": 2521976,
+    //     "company": 441862,
+    //     "itemid": "SKU00006",
+    //     "product_name": "Raw Material 3",
+    //     "to_show": 1,
+    //     "hsn_code": "",
+    //     "type": "Buy",
+    //     "unit": "Kg",
+    //     "category": null,
+    //     "stock": 0,
+    //     "reject_stock": null,
+    //     "price": 100,
+    //     "min_sl": null,
+    //     "max_sl": null,
+    //     "cp_code": "",
+    //     "is_service": 0,
+    //     "drawing_no": "",
+    //     "serial_no": "",
+    //     "custom_fields": {},
+    //     "taxes": null,
+    //     "history_exists": true,
+    //     "other_prices": {},
+    //     "last_modified_by": "Arnav Deep",
+    //     "last_modified_date": "28/12/2022 - 12:46",
+    //     "change_quantity": 1,
+    //     "adjustment_type": "Stock Return",
+    //     "unit_id": 2594308
+    //   }],
+    //   "action_type": "manual_adjustment"
+    // };
+  
+    // const options = {
+    //   method: "POST",
+    //   headers: headers,
+    //   body: JSON.stringify(body)
+    // };
+  
+    // fetch(url, options)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     // Process the response data here
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //     // Handle the error
+    //     console.error(error);
+    //   });
+  }
+
+  counter();
 
   sr.reveal('.background');
   sr.reveal('.skills');
