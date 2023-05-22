b3201b68adb77c149bc5da1ef082a5db9dcddfee
Arnav Deep <arnavd4@gmail.com>
2023-05-22 15:01:24 +0530

    counter pipedream

--- js/main.js
+++ js/main.js
@@ -55,93 +55,10 @@ $(function() {
 
     console.log('Ssarted')
 
-    const url = 'https://be.letstranzact.com/main/login/password-login/';
-    const headers = {
-      'Content-Type': 'application/json'
-    };
-    const data = JSON.stringify({
-      email: 'arnav+prodata@letstranzact.com',
-      password: '123456'
-    });
-
+    const url = 'https://eohkyxq03yy6i7d.m.pipedream.net';
     fetch(url, {
-      method: 'POST',
-      headers: headers,
-      body: data
+      method: 'GET',
     })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data here
-        console.log(data);
-        return;
-      })
-      .catch((error) => {
-        // Handle any errors here
-        console.log('123434');
-        console.log(error);
-      });
-
-
-    // const url = `${baseURL}/inventory/main-inventory/bulk_manual_adjustment_or_stock_transfer/`;
-  
-    // const headers = {
-    //   "authorization": `fdfdfd ${baseURL}`,
-    //   "content-type": "application/json",
-    // };
-  
-    // const body = {
-    //   "fifo_status": true,
-    //   "from_store": null,
-    //   "to_store": 582317,
-    //   "comment": "",
-    //   "stockData": [{
-    //     "id": 2521976,
-    //     "company": 441862,
-    //     "itemid": "SKU00006",
-    //     "product_name": "Raw Material 3",
-    //     "to_show": 1,
-    //     "hsn_code": "",
-    //     "type": "Buy",
-    //     "unit": "Kg",
-    //     "category": null,
-    //     "stock": 0,
-    //     "reject_stock": null,
-    //     "price": 100,
-    //     "min_sl": null,
-    //     "max_sl": null,
-    //     "cp_code": "",
-    //     "is_service": 0,
-    //     "drawing_no": "",
-    //     "serial_no": "",
-    //     "custom_fields": {},
-    //     "taxes": null,
-    //     "history_exists": true,
-    //     "other_prices": {},
-    //     "last_modified_by": "Arnav Deep",
-    //     "last_modified_date": "28/12/2022 - 12:46",
-    //     "change_quantity": 1,
-    //     "adjustment_type": "Stock Return",
-    //     "unit_id": 2594308
-    //   }],
-    //   "action_type": "manual_adjustment"
-    // };
-  
-    // const options = {
-    //   method: "POST",
-    //   headers: headers,
-    //   body: JSON.stringify(body)
-    // };
-  
-    // fetch(url, options)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     // Process the response data here
-    //     console.log(data);
-    //   })
-    //   .catch(error => {
-    //     // Handle the error
-    //     console.error(error);
-    //   });
   }
 
   counter();
